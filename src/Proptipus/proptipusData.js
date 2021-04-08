23e4acbe498f99fbe491d2df94427e2f6ad7401e
Adam Johnson <adamwjo@gmail.com>
2021-04-08 15:10:19 -0400

    some nit picks

---
+++ src/Proptipus/proptipusData.js
@@ -0,0 +1,87 @@
+export default  {
+    punchline:
+      "Ten-tickles! Get it? Like... ...tentacles...because... please just...please be my friend.",
+    magicNumber: 42,
+    favoriteFishGiOhCard: {
+      name: "Red Eye Tetra",
+      image: "https://aquadiction.world/img/profile/red-eye-tetra1.jpg",
+      type: "[DIVINE-BEAST]",
+      attack: 9001,
+      description:
+        "Robust and lively, Tetra make a great addition to any fish army.",
+    },
+    hatedFishGiOhCard: {
+      name: "No Eye Fsh",
+      image: "https://scx2.b-cdn.net/gfx/news/2017/howdoblindca.jpg",
+      type: "[DIVINE-BEAST]",
+      attack: "How?",
+      description: "What a useless freaking fsh.",
+    },
+    friendsList: [
+      {
+        name: "Poptipus",
+        contact: "RadDad@aol.com",
+        socialMedia: "https://twitter.com/octopusfansclub",
+      },
+      {
+        name: "Abikrill",
+        contact: "SisFish@yahoo.com",
+        socialMedia: "https://en.wikipedia.org/wiki/Krill",
+      },
+      {
+        name: "Axolotl Al",
+        contact: "aLotoAx@gmail.com",
+        socialMedia: "https://twitter.com/axobotl",
+      },
+      {
+        name: "Gillfunkle",
+        contact: "SalmonAndGilfunkle@gmail.com",
+        socialMedia: "https://twitter.com/simongarfunkel?lang=en",
+      },
+    ],
+    favoriteMovies: [
+      {
+        title: "The Gills Have Eyes",
+        genre: "Horror",
+        review: "'Left me with baited breath'",
+      },
+      {
+        title: "Gone With the Fins",
+        genre: "Family",
+        review: "'WWII - an important fishue'",
+      },
+      {
+        title: "Bonnie and Cod",
+        genre: "Family",
+        review: "'Most affable criminals you can sea'",
+      },
+      { title: "Finding Nemo", genre: "Family", review: "'I love this movie'" },
+      {
+        title: "Chumdog Millionaire",
+        genre: "Suspense",
+        review: "'What an oppor-tuna-ty!'",
+      },
+      {
+        title: "12 Angry Fins",
+        genre: "Suspense",
+        review: "`12 angry, shellfish public servants`",
+      },
+      {
+        title: "The Blair Fish Project",
+        genre: "Horror",
+        review: "'They must salmon all their courage...'",
+      },
+      {
+        title: "28 Manta Rays Later",
+        genre: "Horror",
+        review: "'When zombies attack they ray-tion their supplies'",
+      },
+      {
+        title: "Beauty & The Breached Whale",
+        genre: "Family",
+        review: "'He just wants to cuttle'",
+      },
+    ],
+    showMovies: false,
+    moviesFilter: "All",
+  }
